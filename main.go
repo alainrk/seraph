c83f737586100c60fabee2fce939c7fb0290947e
Alain Di Chiappari <alain.devcs@gmail.com>
2021-04-06 20:57:11 +0200

    flags handling

--- main.go
+++ main.go
@@ -11,9 +11,33 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"flag"
 )
 
-func encrypt(keyString string, plaintext string) (encryptedString string) {
+type flags struct {
+	mode string
+	passphrase string
+	__isEncrypt bool
+	__isDecrypt bool
+}
+
+// implement stringFlag to distinguish empty string given
+type stringFlag struct {
+	set   bool
+	value string
+}
+
+func (sf *stringFlag) Set(x string) error {
+	sf.value = x
+	sf.set = true
+	return nil
+}
+
+func (sf *stringFlag) String() string {
+	return sf.value
+}
+
+func encrypt(keyString string, plaintext string) string {
 	key, _ := hex.DecodeString(keyString)
 	plaintextBytes := []byte(plaintext)
 
@@ -38,7 +62,7 @@ func encrypt(keyString string, plaintext string) (encryptedString string) {
 	return fmt.Sprintf("%x", ciphertext)
 }
 
-func decrypt(keyString string, ciphertext string) (decryptedString string) {
+func decrypt(keyString string, ciphertext string) string {
 	key, _ := hex.DecodeString(keyString)
 	enc, _ := hex.DecodeString(string(ciphertext))
 
@@ -60,7 +84,7 @@ func decrypt(keyString string, ciphertext string) (decryptedString string) {
 }
 
 // Prompt the output on stdin and returns the clean input string given from the user
-func prompt(output string) (inputString string) {
+func prompt(output string) string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print(output)
 	text, err := reader.ReadString('\n')
@@ -71,10 +95,58 @@ func prompt(output string) (inputString string) {
 	return text
 }
 
+func getFlags() (flagsObject *flags) {
+	var encryptFlag bool
+	var decryptFlag bool
+	var passphraseFlag stringFlag
+
+	flag.BoolVar(&encryptFlag, "e", false, "encrypt mode")
+	flag.BoolVar(&decryptFlag, "d", false, "decrypt mode")
+	flag.Var(&passphraseFlag, "p", "passphrase [INSECURE method, use interactive mode instead]")
+
+	flag.Parse()
+
+	if encryptFlag && decryptFlag {
+		fmt.Println("Choose only one between [-d] and [-e] modality")
+		os.Exit(1)
+	}
+
+	if !(encryptFlag || decryptFlag) {
+		fmt.Println("Choose at least one between [-d] and [-e] modality")
+		os.Exit(1)
+	}
+
+	flagsObject = &flags{}
+
+	if encryptFlag {
+		flagsObject.mode = "encrypt"
+		flagsObject.__isEncrypt = true
+	}
+	if decryptFlag {
+		flagsObject.mode = "decrypt"
+		flagsObject.__isDecrypt = true
+	}
+
+	if !passphraseFlag.set {
+		// passphrase not given
+	} else if passphraseFlag.value == "" {
+		fmt.Println("Flag [-p] needs a string (passphrase) argument")
+		os.Exit(1)
+	} else {
+		flagsObject.passphrase = passphraseFlag.value
+	}
+
+	fmt.Println(flagsObject)
+	return flagsObject
+}
+
 func main() {
+	getFlags()
+
 	// passphrase := "helloworld"
 	passphrase := prompt("Insert key: ")
 	fmt.Printf("Your key: ---%s---\n", passphrase)
+
 	plaintext := "This is a great secret to keep!"
 
 	keyBytes := sha256.Sum256([]byte(passphrase))
