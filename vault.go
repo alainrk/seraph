2a1c7774e8f80b4bb4c1c21564dea572123dce63
Alain Di Chiappari <alain.devcs@gmail.com>
2021-04-10 06:49:34 +0200

    operations on vault and tests

--- vault.go
+++ vault.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+)
+
+const (
+	notUnmarshaledError string = "Vault is not unmarshaled"
 )
 
 type secret struct {
@@ -11,20 +16,35 @@ type secret struct {
 	Password  string `json:"password"`
 	ApiKey    string `json:"apiKey"`
 	Notes     string `json:"notes"`
-	CreatedAt string `json:"createdAt"`
+	CreatedAt string `json:"createdAt"` // [GO= 2006-01-02T15:04:05-0700] [Java= yyyy-MM-dd'T'HH:mm:ssZ]	[C= %FT%T%z] ISO 8601
 }
 
 type vault struct {
-	Name    string
-	Email   string
-	Secrets []secret `json:"secrets"`
+	__isUnmarshaled bool
+	Secrets         []secret `json:"secrets"`
 }
 
 func (v *vault) unmarshal(jsonString string) {
 	json.Unmarshal([]byte(jsonString), v)
+	v.__isUnmarshaled = true
 }
 
-func (v *vault) marshal() string {
+func (v vault) marshal() string {
 	x, _ := json.Marshal(v)
 	return string(x)
+}
+
+func (v *vault) add(s secret) error {
+	if !v.__isUnmarshaled {
+		return errors.New(notUnmarshaledError)
+	}
+	v.Secrets = append(v.Secrets, s)
+	return nil
+}
+
+func (v vault) len() (int, error) {
+	if !v.__isUnmarshaled {
+		return -1, errors.New(notUnmarshaledError)
+	}
+	return len(v.Secrets), nil
 }
--- vault_test.go
+++ vault_test.go
@@ -3,16 +3,25 @@ package main
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
-func TestVault(t *testing.T) {
+const (
+	dateTimeFormat string = "2006-01-02T15:04:05-0700"
+)
+
+func TestVaultMarshaling(t *testing.T) {
 	v := vault{}
 
-	jsonString := `{ "secrets": [{ "name": "GMAIL", "email": "test@email.com", "username": "alainrk", "password": "foobar", "apiKey": "3894H8ETW", "notes": "This is a secret", "createdAt": "2020-02-12 09:12:32" },{ "name": "JIRA", "email": "test@email.com", "username": "jita", "password": "batterystaple", "apiKey": null, "notes": "Jira secret go on", "createdAt": "2020-02-14 09:12:32" }] }`
+	jsonString := `{
+		"secrets": [
+			{ "name": "GMAIL", "email": "test@email.com", "username": "alainrk", "password": "foobar", "apiKey": "3894H8ETW", "notes": "This is a secret", "createdAt": "2016-01-12T20:04:05-0700" },
+			{ "name": "JIRA", "email": "test@email.com", "username": "jita", "password": "batterystaple", "apiKey": null, "notes": "Jira secret go on", "createdAt": "2020-02-19T15:12:05-0700" }
+		]
+	}`
 
 	v.unmarshal(jsonString)
-
-	fmt.Println("Unmarshaled:", v)
+	// fmt.Println("Unmarshaled:", v)
 
 	given := v.Secrets[0].Name
 	expected := "GMAIL"
@@ -33,7 +42,7 @@ func TestVault(t *testing.T) {
 	}
 
 	marshaled := v.marshal()
-	fmt.Println("Marshaled:", marshaled)
+	// fmt.Println("Marshaled:", marshaled)
 
 	unmarshaledVault := vault{}
 	unmarshaledVault.unmarshal(marshaled)
@@ -42,5 +51,40 @@ func TestVault(t *testing.T) {
 	expected = unmarshaledVault.Secrets[0].Password
 	if given != expected {
 		t.Errorf("Marshal/Unmarshal test failed. Given = %s, Expected = %s", given, expected)
+	}
+}
+
+func TestVaultOperations(t *testing.T) {
+	v := vault{}
+	jsonString := `{ "secrets": [] }`
+	v.unmarshal(jsonString)
+
+	given, _ := v.len()
+	var expected int = 0
+	if given != expected {
+		t.Errorf("Failed vault length retrieval. Given = %d, Expected = %d", given, expected)
+	}
+
+	s := secret{}
+	s.Name = "Lorem"
+	s.Username = "ipsum"
+	s.Email = "dolor@s.it"
+	s.Password = "amet"
+	s.ApiKey = "0398509234"
+	s.Notes = "Test 1"
+	s.CreatedAt = time.Now().Format(dateTimeFormat)
+	v.add(s)
+
+	s.Name = "This is another"
+	s.Username = "Different item from the previous one"
+	v.add(s)
+
+	// fmt.Println("Insert secret:", s)
+	fmt.Println(v)
+
+	given, _ = v.len()
+	expected = 2
+	if given != expected {
+		t.Errorf("Failed vault length retrieval after adding. Given = %d, Expected = %d", given, expected)
 	}
 }
