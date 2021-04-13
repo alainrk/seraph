8d7db8bd7dc6364073f503e1e07ae140eb1ce847
Alain Di Chiappari <alain.devcs@gmail.com>
2021-04-13 20:53:40 +0200

    tests

--- main.go
+++ main.go
@@ -59,6 +59,9 @@ func main() {
 	var ciphertext string
 	var deciphertext string
 
+	// TODO: Non-interactive handling
+	// flags := getFlags()
+
 	mode, err := promptForMode()
 	check(err)
 
@@ -107,45 +110,4 @@ func main() {
 		}
 		fmt.Printf("Decrypted: %s\n", deciphertext)
 	}
-}
-
-// OLD modality -> maybe to consider for non interactive mode
-func flagsHandling() {
-	flags := getFlags()
-	var passphrase string
-	var ciphertext string
-	var deciphertext string
-
-	if flags.passphrase == "" {
-		passphrase = prompt("Insert key: ")
-	} else {
-		passphrase = flags.passphrase
-	}
-
-	key := hashPassphrase(passphrase)
-
-	// Check secret file exists, create otherwise
-	if _, err := os.Stat(SecretFile); err != nil {
-		f, err := os.Create(SecretFile)
-		check(err)
-		// close immediately here
-		f.Close()
-	}
-
-	if flags.encryptMode {
-		plaintext := prompt("Insert secret: ")
-		ciphertext = encrypt(key, plaintext)
-		err := ioutil.WriteFile(SecretFile, []byte(ciphertext), 0644)
-		check(err)
-		fmt.Printf("Encrypted file to %s\n", SecretFile)
-	}
-
-	if flags.decryptMode {
-		dat, err := ioutil.ReadFile(SecretFile)
-		check(err)
-		ciphertext = string(dat)
-		deciphertext, _ = decrypt(key, ciphertext)
-		fmt.Printf("Decrypted: %s\n", deciphertext)
-	}
-
 }
