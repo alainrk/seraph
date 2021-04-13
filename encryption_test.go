5d9e068f87ef04ae9536d8d05f61d6985fbd956d
Alain Di Chiappari <alain.devcs@gmail.com>
2021-04-13 20:52:10 +0200

    tests

--- encryption_test.go
+++ encryption_test.go
@@ -8,7 +8,7 @@ func TestEncryptDecrypt(t *testing.T) {
 	key := hashPassphrase("passphrase")
 	plaintext := "This is a test"
 	ciphertext := encrypt(key, plaintext)
-	deciphertext := decrypt(key, ciphertext)
+	deciphertext, _ := decrypt(key, ciphertext)
 
 	if deciphertext != plaintext {
 		t.Errorf("Error on crypt/decrypt. Expected \"%s\", given \"%s\"", plaintext, deciphertext)
